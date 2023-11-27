7d7f2d32a2b08c5465f4c188435e7c1806186363
samarbaeffruslan <samarbaevri>
2023-11-27 14:18:45 +0300

    SlidingWindow: max_sliding_window

---
+++ src/test/kotlin/practice/sliding_window/sliding_window_maximum/MaxSlidingWindowSolutionDequeTest.kt
@@ -0,0 +1,21 @@
+package practice.sliding_window.sliding_window_maximum
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+internal class MaxSlidingWindowSolutionDequeTest {
+
+    private val solutionDeque = MaxSlidingWindowSolutionDeque()
+
+    @Test
+    fun testCase1() {
+        val testArray = intArrayOf(1,3,-1,-3,5,3,6,7)
+        assertArrayEquals(solutionDeque.maxSlidingWindow(testArray, 3), intArrayOf(3,3,5,5,6,7))
+    }
+
+    @Test
+    fun testCase2() {
+        val testArray = intArrayOf(1)
+        assertArrayEquals(solutionDeque.maxSlidingWindow(testArray,1), intArrayOf(1))
+    }
+}
