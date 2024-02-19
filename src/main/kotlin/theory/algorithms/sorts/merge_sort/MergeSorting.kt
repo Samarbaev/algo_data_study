1b7895d10e517677fdc975c7767e363b5666c1db
Samarbaev <samarbaev36@gmail.com>
2024-02-19 21:49:11 +0300

    theory/merge_sorting
    * Implemented merge sort

---
+++ src/main/kotlin/theory/algorithms/sorts/merge_sort/MergeSorting.kt
@@ -0,0 +1,42 @@
+package theory.algorithms.sorts.merge_sort
+
+
+fun mergeSort(array: List<Int>): List<Int> {
+
+    if (array.size <= 1) {
+        return array
+    }
+    val middle = array.size / 2
+
+    val left = array.subList(0, middle)
+    val right = array.subList(middle, array.size)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+fun merge(leftArray: List<Int>, rightArray: List<Int>): List<Int> {
+    var indexLeft = 0; var indexRight = 0
+    val resultArray = mutableListOf<Int>()
+
+    while (indexLeft < leftArray.size && indexRight < rightArray.size) {
+        if (leftArray[indexLeft] <= rightArray[indexRight]) {
+            resultArray.add(leftArray[indexLeft])
+            indexLeft++
+        } else {
+            resultArray.add(rightArray[indexRight])
+            indexRight++
+        }
+    }
+
+    while (indexLeft < leftArray.size) {
+        resultArray.add(leftArray[indexLeft])
+        indexLeft++
+    }
+
+    while (indexRight < rightArray.size) {
+        resultArray.add(rightArray[indexRight])
+        indexRight++
+    }
+
+    return resultArray
+}
