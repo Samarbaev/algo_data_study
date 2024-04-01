e27a4abd2e6ea375dda2529e06c862a491d51322
Samarbaev <samarbaev36@gmail.com>
2024-04-01 23:10:02 +0300

    solution tasks
    * sprint_4_final_a
    * sprint_4_final_b

--- src/main/kotlin/practice/ya_contest/sprint_4_hash_function_and_hash_map/final/A.kt
+++ src/main/kotlin/practice/ya_contest/sprint_4_hash_function_and_hash_map/final/A.kt
@@ -1,7 +1,7 @@
 package practice.ya_contest.sprint_4_hash_function_and_hash_map.final
 
 /***
- * https://contest.yandex.ru/contest/24414/run-report/110687232/
+ * https://contest.yandex.ru/contest/24414/run-report/111152774/
  *
  * Работа алгоритма:
  * 1) Индексирование
@@ -20,8 +20,14 @@ package practice.ya_contest.sprint_4_hash_function_and_hash_map.final
  * Time complexity: O(nk) - индексирование, O(n * log(k)) - поиск.
  */
 
+data class Document(
+    val id: Int,
+    val count: Int
+)
+
 fun main() {
     val countDocuments = readLine()!!.toInt()
+    // не понимаю как сразу считать index :(
     val documents = Array(countDocuments) { readLine()!! }
 
     val index = buildIndex(documents)
@@ -34,23 +40,24 @@ fun main() {
     }
 }
 
-fun buildIndex(documents: Array<String>): Map<String, List<Pair<Int, Int>>> {
-    val index = mutableMapOf<String, MutableList<Pair<Int, Int>>>()
+fun buildIndex(documents: Array<String>): Map<String, List<Document>> {
+    val index = mutableMapOf<String, MutableList<Document>>()
     documents.forEachIndexed { docId, text ->
         val wordFrequency = text.split(" ").groupingBy { it }.eachCount()
         wordFrequency.forEach { (word, frequency) ->
-            index.getOrPut(word) { mutableListOf() }.add(Pair(docId, frequency))
+            index.getOrPut(word) { mutableListOf() }.add(Document(docId, frequency))
         }
     }
     return index
 }
 
-fun findMostRelevantDocuments(query: String, index: Map<String, List<Pair<Int, Int>>>): List<Int> {
+fun findMostRelevantDocuments(query: String, index: Map<String, List<Document>>): List<Int> {
     val relevanceScores = mutableMapOf<Int, Int>()
     query.split(" ").distinct().forEach { word ->
         index[word]?.forEach { (docId, frequency) ->
-            relevanceScores[docId] = relevanceScores.getOrDefault(docId, 0) + frequency
+            relevanceScores.merge(docId, frequency) { value, count  -> value + count }
         }
     }
+    // как улучшить за линию тоже не понимаю(
     return relevanceScores.toList().sortedWith(compareByDescending<Pair<Int, Int>> { it.second }.thenBy { it.first }).map { it.first + 1 }.take(5)
 }
